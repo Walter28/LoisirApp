98cab3846a64b4df87b35e07a8217bc8353433ce
Walter28 <walterchristian28@gmail.com>
2024-06-12 09:57:51 +0200

    creation of updateUser php file

--- controllers/updateUtilisateur.php
+++ controllers/updateUtilisateur.php
@@ -5,6 +5,8 @@ include '../configuration/config.php';
 include '../models/Utilisateur.php';
 
 print_r($_POST);
+print_r($_FILES);
+if($_FILES['profil_utilisateur']['name'] == "") echo "nothin''''";
 die;
 
 
@@ -14,29 +16,30 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nom_utilisateur = htmlspecialchars($_POST['nom_utilisateur']);
     $prenom_utilisateur = htmlspecialchars($_POST['prenom_utilisateur']);
     $profil_utilisateur = $_FILES['profil_utilisateur']['name'];
+    if($_FILES['profil_utilisateur']['name'] == "") $profil_utilisateur = htmlspecialchars($_POST['profil_utilisateur_old']);
     $email_utilisateur = htmlspecialchars($_POST['email_utilisateur']);
     $phone_utilisateur = htmlspecialchars($_POST['phone_utilisateur']);
     $pwd_utilisateur = htmlspecialchars($_POST['pwd_utilisateur']);
+    if($_POST['$pwd_utilisateur'] == "") $pwd_utilisateur = htmlspecialchars($_POST['pwd_utilisateur_old']);
+    else $pwd_utilisateur = md5($pwd_utilisateur);
     $date_creation_compte = null;
     $status_compte = "client";
 
-    $additional = null;
-    $description_utilisateur = null;
-    $link_fb = null;
-    $link_insta = null;
-    $link_x = null;
-    $link_yout = null;
-    $job_utilisateur = null;
-    $year_xperia_utilisateur = null;
-    $address_utilisateur = null;
-
-    $pwd_utilisateur = md5($pwd_utilisateur);
+    $additional = htmlspecialchars($_POST['additional']);
+    $description_utilisateur = htmlspecialchars($_POST['description_utilisateur']);
+    $link_fb = htmlspecialchars($_POST['link_fb']);
+    $link_insta = htmlspecialchars($_POST['link_insta']);
+    $link_x = htmlspecialchars($_POST['link_x']);
+    $link_yout = htmlspecialchars($_POST['link_yout']);
+    $job_utilisateur = htmlspecialchars($_POST['job_utilisateur']);
+    $year_xperia_utilisateur = htmlspecialchars($_POST['year_xperia_utilisateur']);
+    $address_utilisateur = htmlspecialchars($_POST['address_utilisateur']);
 
     $user = new Utilisateur($nom_utilisateur, $prenom_utilisateur, $profil_utilisateur, $email_utilisateur, $phone_utilisateur,
     $pwd_utilisateur, $date_creation_compte, $status_compte, $additional, $description_utilisateur, $link_fb, $link_insta,
     $link_x, $link_yout, $job_utilisateur, $year_xperia_utilisateur, $address_utilisateur);
 
-    if ($user->enregistrerUtilisateur()) {
+    if ($user->updateUtilisateur($_SESSION['userid'])) {
         $_SESSION['message'] = 200;
         header("Location:../login.php?success");
     } else {
