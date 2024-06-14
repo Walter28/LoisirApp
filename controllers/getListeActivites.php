75c34f1bbcf9db4902b534f6a8c3ad1d0520ff56
Walter28 <walterchristian28@gmail.com>
2024-06-14 11:22:03 +0200

    comment getListeActivites, not necesery here

--- controllers/getListeActivites.php
+++ controllers/getListeActivites.php
@@ -2,10 +2,10 @@
     include('./configuration/config.php');
     include_once('./models/Activite.php');
 
-    function getListeActivites(){
-        // $annee = $_SESSION['annee2'];
-        return Activite::getActivites();
-    }
+    // function getListeActivites(){
+    //     // $annee = $_SESSION['annee2'];
+    //     return Activite::getActivites();
+    // }
 
     function getActiviteSponsored(){
         return Activite::getSponsoredActivite();
